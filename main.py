312523a77f110dcc0311c84ba110a6569521ca56
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-06-27 10:45:49 -0400

    4-1 commit

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 def main():
     result = []
     while True:
-        start = input('Enter the starting letter: ')
-        end = input('Enter the starting letter: ')
+        start = input('Enter the starting letter: ').lower()
+        end = input('Enter the starting letter: ').lower()
+        if not (start.isalpha() and end.isalpha()):
+            print("Error: Both inputs must be letters.")
+            continue
+        if len(start) != 1 or len(end) != 1:
+            print("Error: both must be single letters.")
+            continue
+        if ord(start) > ord(end):
+            print("Error: the start letter must be less than the end letter.")
+            continue
+        break
+    for char_code in range(ord(start), ord(end) + 1):
+        result.append(chr(char_code))
+    
 
     """
     ########################################
